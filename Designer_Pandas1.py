30a84b13bc2926847bc3c7f0fd5ea1b2be444615
Samir P Nimgade <samirnimgade76@gmail.com>
2026-06-26 20:23:36 +0530

    Update Designer_Pandas1.py

--- Designer_Pandas1.py
+++ Designer_Pandas1.py
@@ -14,22 +14,3 @@ print("slicing using rows and columns \n",df.iloc[0:5, 0:4])
 print("categorical columns valu count:\n", df['Game'].value_counts())
 
 print("Filtering\n", df[df['Swag_Score'] > 9.5])
-
-df['Kills'] = df['Kills'].fillna(df['Kills'].median()) # fill null with median
-print(f"Nulls\n {df.isnull().sum()}") # count nulls
-
-df_cleaned = df.dropna() # drop nulls
-print(f"Original rows: {len(df)} | Cleaned rows: {len(df_cleaned)}")
-
-df.describe()
-
-len(df)
-
-df_array = df.values # convert a Dataframe into an Array - from np to pd
-print(df_array[:2])
-
-df_encoded = pd.get_dummies(df['Game'])
-display(df_encoded.head())
-
-df['Criminality'] = df['Kills']/df['Hours_Played']
-print(df[df['Criminality']>20])
